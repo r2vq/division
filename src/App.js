7e58a96f3ee0b0a499b1797ea3a9218ee5652399
Keane Quibilan <arv.r2vq@gmail.com>
2023-01-01 01:45:47 -0500

    Add purchases to screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import Purchase from './component/Purchase';
 import sample from "./sample.json";
 
 function App() {
-  console.log(sample);
+  const [purchases, setPurchases] = useState([]);
+
+  useEffect(() => {
+    setPurchases(sample.purchases);
+  }, []);
+
   return (
-    <></>
+    <>
+      <div className="purchases">
+        {purchases.map(purchase => (
+          <Purchase
+            key={purchase.id}
+            name={purchase.name}
+            amount={purchase.amount}
+            spender={purchase.spender}
+          />
+        ))}
+      </div>
+    </>
   );
 }
 
